a83e27faeae65b6f1ea382d725644e7ec1cf0c88
Dario Thornhill <dario.thornhill@gmail.com>
2019-04-13 16:36:10 -0400

    Added demo files

---
+++ class-01-toolbelt-terminal-git/demo/io/app.js
@@ -0,0 +1,9 @@
+var person;
+
+var number;
+number = 5;
+
+person = prompt('Enter a name');
+
+console.log('Hello ' + person + '!');
+alert(`Hello ${person}!`);
---
+++ class-01-toolbelt-terminal-git/demo/semantic/README.md
@@ -0,0 +1,2 @@
+# a-new-repo1
+# a-new-line
