bc20df33810e6acaad0b048222edea6801b70ae6
Dario Thornhill <dario.thornhill@gmail.com>
2019-04-20 18:50:36 -0400

    Cleaned up code demo

--- class-04-links-functions-layouts/code-demo/js/app.js
+++ class-04-links-functions-layouts/code-demo/js/app.js
@@ -1,53 +1,54 @@
 'use strict';
 
 //Push and pop work on the end fo the array
-// var cats = [];
-// console.log({ cats });
-// var response = prompt('What\'s a good name for a cat', 'Type a cat\'s name');
-// cats.push(response);
 
-// var response = prompt('What\'s a good name for a cat', 'Type a cat\'s name');
-// cats.push(response);
-// console.log({ cats });
+var cats = [];
+console.log(cats);
 
-// var currentCat = cats.pop();
+cats.push('Tabby');
+cats.push('Boar');
+console.log(cats);
 
-// alert(currentCat);
-// console.log({ cats });
+var currentCat = cats.pop();
+console.log(currentCat);
+console.log(cats);
 
 // shift and unshift work on the front of the array
 
-// var dogs = [];
-// console.log(dogs);
+var dogs = [];
+console.log(dogs);
 
-// dogs.unshift(prompt('Dog Name'));
-// console.log(dogs);
-// dogs.unshift(prompt('Dog Name'));
-// console.log(dogs);
+dogs.unshift('Butch');
+console.log(dogs);
+dogs.unshift('CAssidy');
+console.log(dogs);
 
-// var current = dogs.shift();
-// console.log(current);
+var current = dogs.shift();
+console.log(current);
 
 //Filling an array with a value
-// var cars = ['Nissan', 'Suzuki', 'Toyota', 'Audi', 'BWM'];
-// console.log(cars);
-// cars.fill('Lexus');
-// console.log(cars);
-// cars.fill('Tesla', 1, 3);
-// console.log(cars);
-// cars.fill('Skoda', cars.length - 2);
-// console.log(cars);
+var cars = ['Nissan', 'Suzuki', 'Toyota', 'Audi', 'BWM'];
+console.log(cars);
+cars.fill('Lexus');
+console.log(cars);
+cars.fill('Tesla', 1, 3);
+console.log(cars);
+cars.fill('Skoda', cars.length - 2);
+console.log(cars);
 
 //Splice can remove or insert an element at a specific position or range
-// var animals = ['cat', 'more cats', 'dogs', 'monkeys', 'deer', 'horses'];
-// animals.splice(0, 2);
-// console.log(animals);
-// animals.splice(1, 1, 'cats', 'more cats');
-// console.log(animals);
+var animals = ['cat', 'more cats', 'dogs', 'monkeys', 'deer', 'horses'];
+//Starting at positions 0 delete 2 entries
+animals.splice(0, 2);
+console.log(animals);
+//Starting at position 1 delete one item then add cats and more cats
+animals.splice(1, 1, 'cats', 'more cats');
+console.log(animals);
 
-//Split a list stored as a string into ana array
+//Split a string into an array
 
 var shows = 'Game of Thrones, The 100, Dark Matter';
+// by cutting at a specific character
 var showsArr = shows.split(',');
 console.log(showsArr);
 
@@ -71,9 +72,9 @@ function renderOne(item) {
 }
 
 renderOne('Damien'); //Damien
-renderOne('Dario'); //Tabby
-renderOne('Kara'); //Butch
-renderOne('Halle'); //Avengers
+renderOne('Dario'); //Dario
+renderOne('Kara'); //Kara
+renderOne('Halle'); //Halle
 
 function renderAll(param) {
   for (var i = 0; i < param.length; i++) {
